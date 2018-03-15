50faaa634b59adbb9ed69a0c6544e5645e5df371
Arne Olai Lyngsmo Molland <arneolaim@gmail.com>
2018-03-15 02:24:01 +0100

    test

--- clima-station/sensetest.py
+++ clima-station/sensetest.py
@@ -3,4 +3,17 @@ from sense_hat import SenseHat
 sense = SenseHat()
 
 while True:
-    sense.show_message("Ur mom gey..", scroll_speed=0.2, text_colour=[255, 255, 0])
+
+    sense.clear()
+    pressure = sense.get_pressure()
+    sense.show_message('p: {0}'.format(pressure))
+
+    sense.clear()
+    temp = sense.get_temperature()
+    sense.show_message('t: {0}'.format(temp))
+
+    sense.clear()
+    humidity = sense.get_humidity()
+    sense.show_message('h: {0}'.format(humidity))
+
+
