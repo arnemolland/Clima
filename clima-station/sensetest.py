b0902096e004d183668086d8a44f9a512118f216
Arne Olai Lyngsmo Molland <arneolaim@gmail.com>
2018-03-15 02:18:02 +0100

    test

--- clima-station/sensetest.py
+++ clima-station/sensetest.py
@@ -3,4 +3,4 @@ from sense_hat import SenseHat
 sense = SenseHat()
 
 while True:
-    sense.show_message("Hello world", scroll_speed=0.2, text_colour=[255, 255, 0])
+    sense.show_message("Ur mom gey..", scroll_speed=0.2, text_colour=[255, 255, 0])
