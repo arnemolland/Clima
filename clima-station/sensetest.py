d15ed70227fc35d179b1f4048aeba5923e8d13b8
Arne Olai Lyngsmo Molland <arneolaim@gmail.com>
2018-03-15 12:49:03 +0100

    skrt

--- clima-station/sensetest.py
+++ clima-station/sensetest.py
@@ -6,14 +6,17 @@ while True:
 
     sense.clear()
     pressure = sense.get_pressure()
-    sense.show_message('p: {0}:.5'.format(pressure))
+    sense.show_message('p: ')
+    sense.show_message(':.5'.format(pressure))
 
     sense.clear()
     temp = sense.get_temperature()
-    sense.show_message('t: {0}:.5'.format(temp))
+    sense.show_message('t: ')
+    sense.show_message(':.5'.format(temp))
 
     sense.clear()
     humidity = sense.get_humidity()
-    sense.show_message('h: {0}:.5'.format(humidity))
+    sense.show_message('h: ')
+    sense.show_message(':.5'.format(humidity))
 
 
